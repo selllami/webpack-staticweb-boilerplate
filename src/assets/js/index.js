57db8e5dcdbee41c24abeb96c268734524160f99
Yassine <yassine.Selllami@gmail.com>
2020-05-29 23:20:33 +0200

    Webpack Asset management

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,3 @@
+import '../css/style.css';
+
 console.log('hello world');
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,41 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/assets/js/index.js',
+  //https://webpack.js.org/concepts/#entry
+  entry: {
+    index:  './src/assets/js/index.js'
+  },
+
+  //https://webpack.js.org/concepts/#output
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  
+  //https://webpack.js.org/concepts/#loaders
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      // Loading Images
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      // Loading Fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
   },
 };
